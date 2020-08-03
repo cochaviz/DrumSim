f995035c33d5104d45f195f6bbf8cc7f3779862f
Zohar Cochavi <zohar.cochavi@gmail.com>
2020-08-03 10:27:17 +0200

    Initial commit

---
+++ Assets/Scripts/CenterOfMass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterOfMass : MonoBehaviour
+{
+    public Vector3 newCenterOfMass;
+    protected Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.centerOfMass = newCenterOfMass;
+        
+    }
+}
---
+++ Assets/Scripts/ClampRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClampRotation : MonoBehaviour
+{
+    public float torque;
+    public float threshold;
+    Vector3 startRotation;
+    Rigidbody rigidBody;
+    void Start()
+    {
+        startRotation = transform.rotation.eulerAngles;
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        Vector3 difference = (transform.rotation.eulerAngles - startRotation);
+        if (Mathf.Abs(difference.magnitude) > threshold)
+        {
+            rigidBody.AddTorque(torque * difference);
+        } 
+    }
+}
